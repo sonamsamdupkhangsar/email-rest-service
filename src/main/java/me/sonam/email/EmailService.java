e3e12534ba28f7dc0480861dbc129593868825dd
Sonam Samdupkhangsar <sonamhava@gmail.com>
2023-03-26 14:37:43 -0600

    Feature/discovery service (#11)
    * add discovery service for localhost
    
    * send a test email on postconstruct
    
    * add EUREKA_HOST as env
    
    ---------
    
    Co-authored-by: Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>

--- src/main/java/me/sonam/email/EmailService.java
+++ src/main/java/me/sonam/email/EmailService.java
@@ -3,9 +3,12 @@ package me.sonam.email;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Business service that sends email using autoconfigured {@link JavaMailSender}
@@ -16,6 +19,9 @@ public class EmailService {
 
     @Autowired
     private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String emailUser;
 
     public void sendEmail(String from, String to, String subject, String body) {
         LOG.info("send email with javaMailSender: from: {},\n to: {},\n subject: {}, body: {}",
@@ -30,5 +36,17 @@ public class EmailService {
 
         javaMailSender.send(msg);
         LOG.info("email sent");
+    }
+
+    @PostConstruct
+    public void sendEmailOnConstruct() {
+        LOG.info("send email on post construct");
+        try {
+            sendEmail(emailUser, "test1@sonam.email", "hello, testing", "hello testing body");
+        }
+        catch (Exception e) {
+            LOG.error("failed to send email: {}", e.getMessage());
+            throw e;
+        }
     }
 }

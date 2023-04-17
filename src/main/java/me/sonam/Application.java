7dc0f2c321e2e97b03456a15de7134f92b69c030
Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>
2023-04-17 08:48:29 -0600

    remove cors and add caffeince cache

--- src/main/java/me/sonam/Application.java
+++ src/main/java/me/sonam/Application.java
@@ -5,12 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @EnableEurekaClient
 @SpringBootApplication(scanBasePackages = {"me.sonam"})
@@ -19,23 +13,5 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-    @Bean
-    CorsWebFilter corsWebFilter() {
-        LOG.info("allow cors filter");
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setMaxAge(8000L);
-        //these origins are needed for browser to communicate with service
-        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:8061", "https://swaggerui.sonam.cloud"));
-        corsConfig.addAllowedMethod("*");
-        corsConfig.addAllowedHeader("Content-Type");
-        corsConfig.addAllowedHeader("api_key");
-        corsConfig.addAllowedHeader("Authorization");
-
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
-
-        return new CorsWebFilter(source);
     }
 }
